c34f98aa16dcd4ed34057144792fd78339397753
Andres Erbsen <andreser@yahoo-inc.com>
2015-07-06 17:27:00 +0000

    LogReplicator interface: elaborate

--- server/replication/replication.go
+++ server/replication/replication.go
@@ -4,19 +4,26 @@ import (
 	"github.com/yahoo/coname/internal/golang.org/x/net/context"
 )
 
-// LogReplicator is a generic interface to state-machine replication logs.
-// The log is a mapping from uint64 slot indices to []byte entries.  Start(lo)
-// must be called exactly once before any other method is called. The other
-// three methods may be called concurrently. This interface here does not
-// support log compaction.
+// LogReplicator is a generic interface to state-machine replication logs.  The
+// log is a mapping from uint64 slot indices to []byte entries in which all
+// entries that have been committed are reliably persistent even throughout
+// machine crashes and data losses at a limited number of replicas. This is
+// achieved by trading off availability: proposing a new netry does not
+// necessarily mean it will be committed. One would use this interface
+// similarly to a local write-ahead log, except that this interface does not
+// support log compaction (it is intended for use when the entire log needs to
+// be kept around anyway).
+// Start(lo) must be called exactly once before any other method is called, and
+// no methods must be called after Close is called. The other three methods may
+// be called concurrently.
 type LogReplicator interface {
 	// Start sets an internal field lo; later WaitCommitted will return entries
-	// with indices >= lo.
+	// with indices >= lo. Start must be called before any other methods are.
 	Start(lo uint64) error
 
 	// Propose moves to append data to the log. It is not guaranteed that the
-	// entry will get appended, though, due to node or network failures
-	// data : *mut // ownership is transferred to LogReplicator
+	// entry will get appended, though, due to node or network failures.
+	// data : []*mut // ownership of the slice contents is transferred to LogReplicator
 	Propose(ctx context.Context, data []byte)
 
 	// GetCommitted loads committed entries for post-replication distribution:
@@ -30,7 +37,7 @@ type LogReplicator interface {
 	// WaitCommitted returns a channel that returns new committed entries,
 	// starting with the index passed to Start.
 	// All calls return the same channel.
-	// ch : chan (*mut Entry) // reader owns read values
+	// ch : chan (&[]byte) // all read values are read-only to the caller
 	WaitCommitted() <-chan []byte
 
 	// Close cleanly stops logging requests. No calls to Propose or
