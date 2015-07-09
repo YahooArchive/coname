74d55ffe7efd64fb319ee94986f569932f32a27a
Andres Erbsen <andreser@yahoo-inc.com>
2015-07-09 00:50:23 +0000

    common/policy.go: fix Errorf calls in

--- common/policy.go
+++ common/policy.go
@@ -8,13 +8,14 @@ import (
 
 // VerifyUpdate returns nil iff replacing entry current (nil if none) with next
 // is justified given the evidence in update. Globally deterministic.
-func VerifyUpdate(current *proto.Entry, update *proto.SignedEntryUpdate, next *proto.Entry) error {
+func VerifyUpdate(current *proto.Entry, update *proto.SignedEntryUpdate) error {
+	next := &update.Update.NewEntry
 	if current != nil {
 		if current.UpdateKey == nil {
 			return fmt.Errorf("VerifyUpdate: current.UpdateKey is nil")
 		}
 		if !VerifySignature(current.UpdateKey, update.Update.PreservedEncoding, update.OldSig) {
-			return fmt.Errorf("VerifyUpdate: replacing an entry requires authorization from the old key, but signature verification failed", next.Version, current.Version)
+			return fmt.Errorf("VerifyUpdate: replacing an entry requires authorization from the old key, but signature verification failed")
 		}
 		if next.Version < current.Version {
 			return fmt.Errorf("VerifyUpdate: entry version must not decrease (got %d < %d)", next.Version, current.Version)
@@ -24,7 +25,7 @@ func VerifyUpdate(current *proto.Entry, update *proto.SignedEntryUpdate, next *p
 		return fmt.Errorf("VerifyUpdate: next.UpdateKey is nil")
 	}
 	if !VerifySignature(next.UpdateKey, update.Update.PreservedEncoding, update.NewSig) {
-		return fmt.Errorf("VerifyUpdate: update needs to be accepted by the new key, but signature verification failed", next.Version, current.Version)
+		return fmt.Errorf("VerifyUpdate: update needs to be accepted by the new key, but signature verification failed")
 	}
 	return nil
 }
