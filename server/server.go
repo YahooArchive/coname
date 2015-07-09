3d3eb7c822873d92a89c74dc930ecfa7f40a3777
Andres Erbsen <andreser@yahoo-inc.com>
2015-07-09 00:54:54 +0000

    server code structure (little actual code yet)

--- server/server.go
+++ server/server.go
@@ -2,35 +2,71 @@ package server
 
 import (
 	"crypto/tls"
+	"log"
 	"net"
 	"sync"
 
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+	"github.com/yahoo/coname/common"
 	"github.com/yahoo/coname/proto"
 	"github.com/yahoo/coname/server/replication"
+	"github.com/yahoo/coname/server/replication/leveldblog"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
+// Config encapsulates everything that needs to be specified about a single
+// replica of one realm's keyserver to operate it.
+// TODO: make this a protobuf, Unmarshal from JSON
 type Config struct {
-	UpdateAddr, LookupAddr, VerifierAddr string
-	UpdateTLS, LookupTLS, VerifierTLS    *tls.Config // TODO: make this struct serializable. tls.Config is not
+	LeveldbDir string
 
-	log replication.LogReplicator // TODO: cluster init information?
+	UpdateAddr, LookupAddr, VerifierAddr string
+	UpdateTLS, LookupTLS, VerifierTLS    *tls.Config
+	// FIXME: tls.Config is not serializable, replicate relevant fields
 }
 
+// Keyserver either manages or verifies a single end-to-end keyserver realm.
 type Keyserver struct {
+	db  *leveldb.DB
+	log replication.LogReplicator
+
+	proto.ReplicaState
+
 	updateServer, lookupServer, verifierServer *grpc.Server
 	updateListen, lookupListen, verifierListen net.Listener
 
-	log replication.LogReplicator
-
-	close    chan struct{}
+	stop     chan struct{}
 	waitStop sync.WaitGroup
 }
 
-func Bind(cfg *Config) (ks *Keyserver, err error) {
-	ks = &Keyserver{log: cfg.log, close: make(chan struct{})}
+// Open initializes a new keyserver based on cfg, reads the persistent state and
+// binds to the specified ports. It does not handle input: requests will block.
+func Open(cfg *Config) (ks *Keyserver, err error) {
+	db, err := leveldb.OpenFile(cfg.LeveldbDir, nil)
+	if err != nil {
+		return nil, err
+	}
+	log, err := leveldblog.NewLeveldbLog(db)
+	if err != nil {
+		return nil, err
+	}
+
+	ks = &Keyserver{db: db, log: log, stop: make(chan struct{})}
+
+	switch replicaStateBytes, err := db.Get(table_replicastate, nil); err {
+	case leveldb.ErrNotFound:
+		// ReplicaState zero value is valid initialization
+	case nil:
+		if err := ks.ReplicaState.Unmarshal(replicaStateBytes); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, err
+	}
+
 	if cfg.UpdateAddr != "" {
 		ks.updateServer = grpc.NewServer(grpc.Creds(credentials.NewTLS(cfg.UpdateTLS)))
 		proto.RegisterE2EKSUpdateServer(ks.updateServer, ks)
@@ -60,8 +96,9 @@ func Bind(cfg *Config) (ks *Keyserver, err error) {
 	return ks, nil
 }
 
+/// Start makes the keyserver start handling requests (forks goroutines).
 func (ks *Keyserver) Start() {
-	ks.log.Start(0) // NOTE: when persistent state is implemented, bump this index on every batch sync
+	ks.log.Start(ks.NextIndexLog)
 	if ks.updateServer != nil {
 		go ks.updateServer.Serve(ks.updateListen)
 	}
@@ -72,17 +109,12 @@ func (ks *Keyserver) Start() {
 		go ks.verifierServer.Serve(ks.verifierListen)
 	}
 	ks.waitStop.Add(1)
-	go func() { ks.waitClose(); ks.waitStop.Done() }()
+	go func() { ks.run(); ks.waitStop.Done() }()
 }
 
+/// Stop cleanly shuts down the keyserver and then returns.
+// TODO: figure out what will happen to connected clients?
 func (ks *Keyserver) Stop() {
-	close(ks.close)
-	ks.waitStop.Wait()
-	ks.log.Close()
-}
-
-func (ks *Keyserver) waitClose() {
-	<-ks.close
 	if ks.updateServer != nil {
 		ks.updateServer.Stop()
 	}
@@ -91,5 +123,63 @@ func (ks *Keyserver) waitClose() {
 	}
 	if ks.verifierServer != nil {
 		ks.verifierServer.Stop()
+	}
+	close(ks.stop)
+	ks.waitStop.Wait()
+	ks.log.Close()
+}
+
+// run is the CSP-style main loop of the keyserver. All code critical for safe
+// persistence should be directly in run. All functions called from run should
+// either interpret data and modify their mutable arguments OR interact with the
+// network and disk, but not both.
+func (ks *Keyserver) run() {
+	var step proto.KeyserverStep
+	var wb leveldb.Batch
+	for {
+		select {
+		case <-ks.stop:
+			return
+		case stepBytes := <-ks.log.WaitCommitted():
+			if stepBytes == nil {
+				continue // allow logs to skip slots for inddexing purposes
+			}
+			if err := step.Unmarshal(stepBytes); err != nil {
+				log.Panicf("invalid step pb in replicated log: %s", err)
+			}
+			ks.NextIndexLog++
+			ks.step(&step, &ks.ReplicaState, &wb)
+			wb.Put(table_replicastate, proto.MustMarshal(&ks.ReplicaState))
+			if err := ks.db.Write(&wb, &opt.WriteOptions{Sync: true}); err != nil {
+				log.Panicf("sync step to db: %s", err)
+			}
+			wb.Reset()
+			// TODO: if we handled a client connected directly to us, reply
+		}
+		// TODO: case pushRatification
+	}
+}
+
+// step is called by run and changes the in-memory state. No i/o allowed.
+func (ks *Keyserver) step(step *proto.KeyserverStep, rs *proto.ReplicaState, wb *leveldb.Batch) {
+	// ks: &const
+	// step, rs, wb: &mut
+	switch {
+	case step.Update != nil:
+		if err := common.VerifyUpdate( /*TODO: tree lookup*/ nil, step.Update); err != nil {
+			// TODO: return the client-bound error code
+			return
+		}
+		// TODO: set entry in tree
+		// TODO: update verifier log index structure
+		// TODO: figure out how to do epoch delimiter timers
+		rs.PendingUpdates = true
+	case step.EpochDelimiter != 0:
+		rs.LastEpochDelimiter = step.EpochDelimiter
+		rs.PendingUpdates = false
+		rs.NextEpoch++
+		// TODO: sign stuff, return KeyserverStep{ReplicaRatification: }
+	default:
+		log.Panicf("unknown step pb in replicated log: %#v", step)
 	}
 }
---
+++ server/table.go
@@ -0,0 +1,6 @@
+package server
+
+var (
+	table_replicalog   = []byte{'l'}
+	table_replicastate = []byte{'e'}
+)
