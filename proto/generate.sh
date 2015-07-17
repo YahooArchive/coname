ffb28ffabf1e87e13b877b39a838065525503328
Andres Erbsen <andreser@yahoo-inc.com>
2015-07-17 10:10:30 -0700

    use gogoproto recommended import path (still does not work)

--- proto/generate.sh
+++ proto/generate.sh
@@ -15,4 +15,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$DIR"
-protoc --gogofast_out=plugins=grpc:. -I "$GOPATH/src" -I . *.proto
+protoc --gogofast_out=plugins=grpc:. -I . -I "$GOPATH/src/github.com/gogo/protobuf" *.proto
