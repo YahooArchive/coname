1bd64994712cf59558202fde68dc51d97abc1737
Aditya Mahendrakar <a.mahendrakar@yahoo.com>
2016-03-21 09:51:49 -0700

    add TestRealIDToken
    use authenticated git clone for googlesource.com
    
    drop go1.3 support

--- keyserver/oidc/token_test.go
+++ keyserver/oidc/token_test.go
@@ -154,3 +154,19 @@ func TestIDTokenInvalidKey(t *testing.T) {
 		t.Error("validated a token signed with a bogus key")
 	}
 }
+
+func TestRealIDToken(t *testing.T) {
+	url := "https://login.yahoo.com/.well-known/openid-configuration"
+	clientID := "dj0yJmk9Smh3OXBBUUdQb3pBJmQ9WVdrOVFtZHplbFJxTm04bWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1jNw--"
+	c := &Client{DiscoveryURL: url, ClientID: clientID}
+	err := c.FetchPubKeys()
+	if err != nil {
+		t.Errorf("FetchPubKeys() failed for (%s), (%v)", url, err)
+	}
+	tokStr := "eyJhbGciOiJFUzI1NiIsImtpZCI6IjM0NjZkNTFmN2RkMGM3ODA1NjU2ODhjMTgzOTIxODE2YzQ1ODg5YWQifQ.eyJhdWQiOiJkajB5Sm1rOVNtaDNPWEJCVVVkUWIzcEJKbVE5V1Zkck9WRnRaSHBsYkZKeFRtMDRiV05IYnpsTlFTMHRKbk05WTI5dWMzVnRaWEp6WldOeVpYUW1lRDFqTnctLSIsInN1YiI6IkVRMk5UVEhER0lZR0lSREJVTEkzUFE2UEVVIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImlzcyI6Imh0dHBzOi8vYXBpLmxvZ2luLnlhaG9vLmNvbSIsIm5hbWUiOiJhZGl0eWEgbWFoZW5kcmFrYXIiLCJleHAiOjE0NTg1Nzg5MjYsImdpdmVuX25hbWUiOiJhZGl0eWEiLCJsb2NhbGUiOiJlbi1VUyIsImlhdCI6MTQ1ODU3NTMyNiwibm9uY2UiOiJmb29iYXJiYXoiLCJmYW1pbHlfbmFtZSI6Im1haGVuZHJha2FyIiwiZW1haWwiOiJhZGl0eWF0ZXN0MUB5YWhvby5jb20ifQ.YVzwmK4xhJyNSSwPPANv6vpBmOXpF4e0hG9SuvHziYs3AGQpAMFQoWu_3HiXX_t7vL8-EVtKEiG4Mp1M2m2ZZw"
+	_, err = c.VerifyIDToken(tokStr)
+	if err == nil || err.Error() != "token is expired" {
+		t.Error("validated an expired token or unexpected error occured")
+	}
+
+}
