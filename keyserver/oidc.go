ae173c3ce3c5b5527c1834d795ef24613e817695
Aditya Mahendrakar <maditya@yahoo-inc.com>
2016-10-04 13:49:27 -0700

    gofmt fix

--- keyserver/oidc.go
+++ keyserver/oidc.go
@@ -18,7 +18,7 @@ func (ks *Keyserver) OIDCRequest(domain string, uri string) (string, error) {
 		v.Set("redirect_uri", uri)
 		v.Set("scope", oc.scope)
 		// TODO: if this is absolutely required, generate a random one and validate it
-		v.Set("nonce", "foobar") 
+		v.Set("nonce", "foobar")
 		// replace '+' with '%20' due to https://github.com/golang/go/issues/4013
 		return oc.oidcClient.Issuer + "/oauth2/request_auth?" + strings.Replace(v.Encode(), "+", "%20", -1), nil
 
